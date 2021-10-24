f88576137bd32f56fd393efe7e15673e7cc0cd14
poke30744 <poke30744@gmail.com>
2021-10-24 17:22:24 +0900

    support dumping EPG info

--- tstriage/epgstation.py
+++ tstriage/epgstation.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from datetime import datetime, timedelta
-import urllib.request, shutil, json, time
+import urllib.request, urllib.parse, shutil, json, time
 
 class EPGStation:
     def __init__(self, url, cache=None):
@@ -36,3 +36,31 @@ class EPGStation:
             while self.IsBusy(duration=timedelta(seconds=duration)):
                 time.sleep(duration)
             self._notBusyTill = datetime.now() + timedelta(seconds=granularity)
+
+    def GetChannels(self):
+        with urllib.request.urlopen(f'{self.url}/api/channels') as response:
+            channels = json.load(response)
+            return channels
+
+    def GetEPG(self, path, limit=24):
+        with urllib.request.urlopen(f'{self.url}/api/recorded?limit={limit}&offset=0&reverse=false') as response:
+            recorded = json.load(response)
+            for epg in recorded['recorded']:
+                filename = urllib.parse.unquote(epg['filename'])
+                if Path(filename).stem in Path(path).stem:
+                    return epg
+    
+    def GenerateDescription(self, epg, channels, txtPath):
+        with Path(txtPath).open('w', encoding='utf8') as f:
+            print(epg['name'], file=f)
+            print('', file=f)
+            print(epg['description'], file=f)
+            print('', file=f)
+            print(epg['extended'], file=f)        
+            print('', file=f)
+            for item in channels:
+                if item.get('id') == epg['channelId']:
+                    print(f'{item["name"]}', file=f)
+                    break
+            duration = epg['endAt'] - epg['startAt'] 
+            print(f"{time.strftime('%Y-%m-%d %H:%M (%a)', time.localtime(epg['startAt'] / 1000))} ~ {round(duration / 1000 / 60)} mins", file=f)
