613ea53e8a68ca44d0df3919c060794d98a11e12
poke30744 <poke30744@gmail.com>
2022-05-16 15:04:56 +0900

    fix unit tests

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,31 +1,36 @@
 import pytest
 from pathlib import Path
-from tstriage.common import LoadExistingData, ExtractProgramList, ExtractProgram
+from tscutter.common import PtsMap
+from tstriage.common import MarkerMap, ExtractProgramList, ExtractProgram
 
-videoPath = r"C:\Samples\2020年05月23日18時00分00秒-名探偵コナン「小五郎はBARにいる(前編)」[解][字][デ]_HD-1.ts"
-indexPath = r"C:\Samples\_metadata\2020年05月23日18時00分00秒-名探偵コナン「小五郎はBARにいる(前編)」[解][字][デ]_HD-1.ptsmap"
-markerPath = r"C:\Samples\_metadata\2020年05月23日18時00分00秒-名探偵コナン「小五郎はBARにいる(前編)」[解][字][デ]_HD-1.markermap"
+videoPath = Path(r"C:\Samples\2020年05月23日18時00分00秒-名探偵コナン「小五郎はBARにいる(前編)」[解][字][デ]_HD-1.ts")
+indexPath = Path(r"C:\Samples\_metadata\2020年05月23日18時00分00秒-名探偵コナン「小五郎はBARにいる(前編)」[解][字][デ]_HD-1.ptsmap")
+markerPath = Path(r"C:\Samples\_metadata\2020年05月23日18時00分00秒-名探偵コナン「小五郎はBARにいる(前編)」[解][字][デ]_HD-1.markermap")
 
-outputPath = r"C:\Samples\conanProgram.ts"
+outputPath = Path(r"C:\Samples\conanProgram.ts")
 
-def test_ExtractProgramList():    
-    _, markerMap = LoadExistingData(Path(indexPath), Path(markerPath))
+def test_ExtractProgramList():
+    markerMap = MarkerMap(markerPath, PtsMap(indexPath))
     programList = ExtractProgramList(markerMap, byGroup=False)
     assert len(programList) == 1
 
 def test_ExtractProgramList_ByGroup():
-    _, markerMap = LoadExistingData(Path(indexPath), Path(markerPath))
+    markerMap = MarkerMap(markerPath, PtsMap(indexPath))
     programList = ExtractProgramList(markerMap, byGroup=True)
     assert len(programList) > 1
 
 def test_ExtractProgram():
-    ptsMap, markerMap = LoadExistingData(Path(indexPath), Path(markerPath))
+    ptsMap = PtsMap(indexPath)
+    markerMap = MarkerMap(markerPath, ptsMap)
     programList = ExtractProgramList(markerMap, byGroup=False)
-    ExtractProgram(videoPath=videoPath, clips=programList[0], ptsMap=ptsMap, outputPath=outputPath)
+    ExtractProgram(videoPath=videoPath, clips=programList[0], ptsMap=ptsMap.data, outputPath=outputPath)
+    assert outputPath.exists()
+    outputPath.unlink()
 
 def test_ExtractProgram_ByGroup():
-    ptsMap, markerMap = LoadExistingData(Path(indexPath), Path(markerPath))
+    ptsMap = PtsMap(indexPath)
+    markerMap = MarkerMap(markerPath, ptsMap)
     programList = ExtractProgramList(markerMap, byGroup=True)
     for i in range(len(programList)):
-        ExtractProgram(videoPath=videoPath, clips=programList[i], ptsMap=ptsMap,
-            outputPath=Path(outputPath).with_stem(f'{Path(outputPath).stem}_{i}'))
+        outputPathN = Path(outputPath).with_stem(f'{Path(outputPath).stem}_{i}')
+        ExtractProgram(videoPath=videoPath, clips=programList[i], ptsMap=ptsMap.data, outputPath=outputPathN)
